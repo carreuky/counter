9b6f529991b71f8693628740a55418122ea7f16e
carreuky <carreuky@gmail.com>
2022-06-23 12:57:56 +0300

    create event to add 10

--- assets/main.js
+++ assets/main.js
@@ -6,3 +6,10 @@ button5.addEventListener('click', () => {
   disp.textContent = "added 5"
   indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
 })
+const button10= document.querySelector('#plusTen')
+
+button10.addEventListener('click', ()=>{
+  disp.textContent='added 10'
+  indicator.textContent = (parseInt(indicator.textContent) + 10).toString()
+
+})
